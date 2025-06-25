7ebba8972ca39d76b0bd47832eb6a415252ea0c8
Steve Calvert <steve.calvert@glean.com>
2025-06-25 12:26:20 -0700

    fix: Fixes deployment configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,17 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://aureliawang-glean.github.io',
+  url: 'https://gleanwork.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/glean-developer-site-wip/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'askscio', // Usually your GitHub org/user name.
-  projectName: 'aureliawang-glean.github.io', // Usually your repo name.
-
-  deploymentBranch: 'gh-pages',
+  organizationName: 'gleanwork', // Usually your GitHub org/user name.
+  projectName: 'glean-developer-site-wip', // Usually your repo name.
 
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',

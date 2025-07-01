45d63f900706fbb27a558c2376b3a2bde1b8f2e6
Steve Calvert <steve.calvert@glean.com>
2025-07-01 08:06:13 -0700

    chore: Remove 'edit this page' links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,10 +44,6 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           routeBasePath: '/',
           docItemComponent: '@theme/ApiItem',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',

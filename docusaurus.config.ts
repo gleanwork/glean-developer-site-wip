4ec585677407b7b3d8c36087ce7702d5793f51da
Steve Calvert <steve.calvert@glean.com>
2025-07-08 16:28:44 -0700

    fix: Fixes connector for search to point to production site connector (#55)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -179,7 +179,7 @@ const config: Config = {
         searchOptions: {
           backend: 'https://glean-public-external-be.glean.com',
           webAppUrl: 'https://glean-public-external.glean.com',
-          datasourcesFilter: ['webxtp3t2lgleandeveloperdocs'],
+          datasourcesFilter: ['webj88x6azgleandeveloperdocs'],
         },
         chatOptions: false,
         enableAnonymousAuth: true,
